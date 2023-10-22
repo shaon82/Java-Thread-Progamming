30ec96a0b2a65c16ef4a053374e86e41d79359f2
shaon82 <shaon82@users.noreply.github.com>
2023-10-22 12:46:53 +0600

    Update Main.java

--- java-thread-join/Main.java
+++ java-thread-join/Main.java
@@ -1,1 +1,33 @@
+public class Main {
+    public static void main(String[] args) {
 
+        DownloadingHeartBeat downloadingHeartBeat = new DownloadingHeartBeat();
+
+        CounterOne  counterOne = new CounterOne();
+
+        CounterTwo counterTwo = new CounterTwo();
+
+
+        counterOne.start();
+        counterTwo.start();
+
+        downloadingHeartBeat.start();
+
+
+        try {
+            counterOne.join();
+            counterTwo.join();
+
+            downloadingHeartBeat.shutDown();
+            downloadingHeartBeat.join();
+
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+
+
+
+        System.out.println("Whole Program is completed!!!!!!");
+    }
+}
