87fc0fef391a27ffed66365240358c86a8b28b3f
shaon82 <shaon82@users.noreply.github.com>
2023-10-22 12:46:06 +0600

    Update Main.java

--- thread-exception-handle/Main.java
+++ thread-exception-handle/Main.java
@@ -1,1 +1,25 @@
+import java.util.concurrent.TimeUnit;
 
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("current thread: "+Thread.currentThread().getName());
+
+        Thread thread = new Thread(new Runnable(){
+
+            @Override
+            public void run() {
+                System.out.println("sum = "+(100/0));
+//                try {
+//                    TimeUnit.SECONDS.sleep(5);
+//                } catch (InterruptedException e) {
+//                    throw new RuntimeException("Something went wrong");
+//                }
+            }
+        });
+
+        thread.setUncaughtExceptionHandler(new CustomUncaughtExceptionHandler());
+
+        thread.start();
+    }
+}
